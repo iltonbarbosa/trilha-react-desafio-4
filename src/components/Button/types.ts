631a9093e3583849c4042e6de6c75c64e1cb98d1
ilton barbosa <ilton.barbosa@embrapa.br>
2024-11-04 08:41:14 -0300

    first commit

---
+++ src/components/Button/types.ts
@@ -0,0 +1,5 @@
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    title: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled: boolean;
+}
